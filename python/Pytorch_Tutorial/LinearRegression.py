0dc0313d3f21f9a5502cf76140819189897f48d1
lvtyeflymoya <Zhanglele0526@126.com>
2024-07-23 16:17:00 +0800

    first commit

---
+++ python/Pytorch_Tutorial/LinearRegression.py
@@ -0,0 +1,48 @@
+import torch
+
+
+# print(torch.cuda.is_available())
+# print(torch.__version__)
+
+x_data = torch.tensor([[1.0], [2.0], [3.0]])
+y_data = torch.tensor([[2.0], [4.0], [6.0]])
+
+
+
+class LinearModel(torch.nn.Module):
+    def __init__(self):
+        # 调用父类（LinearModel的父类）的初始化方法
+        super(LinearModel, self).__init__()
+
+        # 创建一个线性层，输入特征数为1，输出特征数也为1，包含了权重和偏置项
+        self.linear = torch.nn.Linear(1, 1)  # weights and bias
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+model = LinearModel()
+
+# criterion = torch.nn.MSELoss(size_average=False,reduce='sum')
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(),lr=0.1)
+
+for epoch in range(500):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()    # 更新权重
+
+# output weights and bias
+print('w = ',round(model.linear.weight.item(),6))
+print('b = ',round(model.linear.bias.item(),6))
+
+# print('w = ',model.linear.weight.item())
+# print('b = ',model.linear.bias.item())
+
+# test model
+x_test = torch.tensor([[4.0]])
+y_test = model(x_test)
+print('y_pred = ',y_test.data)
+
---
+++ python/Pytorch_Tutorial/LogiticRegression.py
@@ -0,0 +1,50 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# 训练数据
+x_data = torch.tensor([[1.0], [2.0], [3.0]])
+y_data = torch.tensor([[0], [0], [1]])
+
+
+class LogisticRegressionModel(torch.nn.Module):
+    '''逻辑回归模型'''
+    def __init__(self):
+        super(LogisticRegressionModel, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        x = x.float()
+        y_pred = torch.sigmoid(self.linear(x))
+        return y_pred
+model = LogisticRegressionModel()
+
+criterion = torch.nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+for epoch in range(10000):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data.float())
+    print(epoch, loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+x_test = torch.tensor([[4.0]])
+y_test = model(x_test)
+print('y_pred = ',y_test.data)
+
+
+x = np.linspace(0, 10, 200)
+x_t = torch.tensor(x).view((200,1))
+y_t = model(x_t)
+y = y_t.data.numpy()
+plt.plot(x, y)
+plt.plot([0,10],[0.5,0.5],c='r')
+plt.xlabel('hours')
+plt.ylabel('Probability of Pass')
+plt.grid()
+plt.show()
